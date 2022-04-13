55f793f79f10bdb4fa381c48b45ff9d608ab2045
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-04-13 17:44:57 -0600

    debug

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const project3El = document.getElementById('project3');
 const project4El = document.getElementById('project4');
 
 
-project1El.addEventListener("click", function(event) {
+project1El.addEventListener("click", function() {
     window.open("https://jgault87.github.io/weather-machine/");
 });
 
-project2El.addEventListener("click", function(event) {
+project2El.addEventListener("click", function() {
     window.open("https://github.com/jgault87/city-scapes");
 });
 
-project3El.addEventListener("click", function(event) {
+project3El.addEventListener("click", function() {
     window.open("https://github.com/jgault87/my_destiny_planner");
 });
 
