e4bc2356fbc9ef082a78f722502f579fd6e90b78
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-04-13 17:42:28 -0600

    added links to projects/resume

---
+++ script.js
@@ -0,0 +1,18 @@
+const project1El = document.getElementById('project1');
+const project2El = document.getElementById('project2');
+const project3El = document.getElementById('project3');
+const project4El = document.getElementById('project4');
+
+
+project1El.addEventListener("click", function(event) {
+    window.open("https://jgault87.github.io/weather-machine/");
+});
+
+project2El.addEventListener("click", function(event) {
+    window.open("https://github.com/jgault87/city-scapes");
+});
+
+project3El.addEventListener("click", function(event) {
+    window.open("https://github.com/jgault87/my_destiny_planner");
+});
+
